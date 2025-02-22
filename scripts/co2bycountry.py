2cb2440b009761ef26634b69a7b8bcea61e0d162
jyanga11 <jyanga12@gmail.com>
2025-02-22 13:29:18 -0600

    renamed script folder

--- scripts/co2bycountry.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-file = open("..\data\export_20201228_1748.csv")
--- scripts/country_waste.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
-file = open("../data/country_waste.csv")
-df = pd.read_csv(file)
-
-df_waste = df[['country_name', ]]
-
-print(df.head(5))
